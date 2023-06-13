9bd28483b9395389c2d8d9e15bdfb9d5ac688fdf
matkrasinski <mat.krasinski@o2.pl>
2023-06-13 14:53:10 +0200

    Predicted top objects

--- src/main/java/pl/ioad/adoto/database/TopObjectRepository.java
+++ src/main/java/pl/ioad/adoto/database/TopObjectRepository.java
@@ -4,8 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import pl.ioad.adoto.database.entity.Building;
-import pl.ioad.adoto.database.entity.River;
+import pl.ioad.adoto.database.entity.sample.Building;
+import pl.ioad.adoto.database.entity.sample.River;
 import pl.ioad.adoto.database.entity.TopEntity;
 
 import java.util.List;
