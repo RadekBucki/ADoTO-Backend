b437eca35882e5afabf375348d0d368835d8cc50
matkrasinski <mat.krasinski@o2.pl>
2023-06-13 14:53:10 +0200

    Predicted top objects

--- src/main/java/pl/ioad/adoto/database/RoadRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.ioad.adoto.database;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.ioad.adoto.database.entity.Road;
-
-public interface RoadRepository extends JpaRepository<Road, Long> {
-}
