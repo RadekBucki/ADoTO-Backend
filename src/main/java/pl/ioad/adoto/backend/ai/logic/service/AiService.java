648e5c011a9528ca59ec032e089230386741be46
matkrasinski <mat.krasinski@o2.pl>
2023-06-16 11:50:57 +0200

    Saving top objects into database

--- src/main/java/pl/ioad/adoto/backend/ai/logic/service/AiService.java
+++ src/main/java/pl/ioad/adoto/backend/ai/logic/service/AiService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.ioad.adoto.backend.layers.Layer;
 import pl.ioad.adoto.communication.ai.AICommunicationFacade;
+import pl.ioad.adoto.communication.ai.exception.AiResponseFailedException;
 import pl.ioad.adoto.communication.ai.model.AiResult;
 import pl.ioad.adoto.database.DBService;
 
@@ -15,9 +16,19 @@ import java.util.Map;
 public class AiService {
 
     private final AICommunicationFacade aiCommunicationFacade;
+    private final DBService dbService;
     private final Map<String, Layer> layers;
 
     public List<List<AiResult>> getAiResults(double width, double minx, double miny, double maxx, double maxy, String layer) {
-        return aiCommunicationFacade.getAiResults(width, minx, miny, maxx, maxy, layers.get(layer).getAiSpell());
+        var aiResults = aiCommunicationFacade.getAiResults(width, minx, miny, maxx, maxy, layers.get(layer).getAiSpell());
+        if (aiResults == null)
+            throw new AiResponseFailedException("Failed occur while generating AI results");
+
+        aiResults.forEach(res -> dbService.savePrediction(
+                res.stream().map(AiResult::x).toList(),
+                res.stream().map(AiResult::y).toList(),
+                miny, minx, maxy, maxx,
+                layer));
+        return aiResults;
     }
 }
